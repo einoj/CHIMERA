bc25c46a6428cf972218a2830251dc6056fd8c7e
einoj <einoj@users.noreply.github.com>
2016-07-12 17:34:37 +0200

    Started working on code for controlling all 12 memories instead of just 1

---
+++ memories.h
@@ -0,0 +1,28 @@
+/* This file defines the pins used by the memories and functions used to initialize the memories.*/
+
+void init_memories(void);
+
+void poweroff_memories(void);
+
+/* Using these macros to enable and disable the chipselect 
+ is more efficent than using a function because it avoids the
+ extra LD and ST instructions see http://www.atmel.com/webdoc/AVRLibcReferenceManual/FAQ_1faq_port_pass.html
+ Usage example: 
+    static const mem mem_dev = {1,1024,256,&PORTB};
+    enable_cs_macro (*mem_dev.port, mem_dev.PIN_CS);
+*/
+#define enable_cs_macro(port,mask) ((port) |= (mask))
+#define disable_cs_macro(port,mask) ((port) ~= (mask))
+
+typedef struct Memory {
+    // chip select pin
+    const uint8_t PIN_CS;
+    // chip VCC enable pin
+    const uint8_t PIN_VCC;
+    // size of device in bytes 
+    const uint32_t size;
+    // size of one page in bytes
+    const uint16_t pageSize;
+    // The port that the memory chip select is connected to
+    volatile uint8_t *pport;
+} mem;
